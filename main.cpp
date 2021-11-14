f163916238449d82d8a8a42a1cb6f39737973893
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-11-14 12:26:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n;
+    cin>>n;
+    int n1=0,n2=1,risultato=0;
+    if (n>=2){
+        cout<<n2<<endl;
+        int i;
+        for(i=1;i<n;i++){
+            risultato=n1+n2;
+            n1=n2;
+            n2=risultato;
+            cout<<risultato<<endl;
+        }
+    }
+    else{
+        cout<<"errore";
+    }
+
+    return 0;
 }
+
